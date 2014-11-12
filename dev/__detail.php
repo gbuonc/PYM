213d72b7a03b2075a000cb731151ad0348c1dd3a
Gianluca Buoncompagni <gbuonc@gmail.com>
2014-11-12 19:09:06 +0100

    migrate to php

---
+++ dev/__detail.php
@@ -0,0 +1,41 @@
+<?php require('inc/head.php');?>
+    <body class="" id="">
+        <div class="main-wrap m-scene" id="main" >
+			<header>
+            	<nav style="text-align:left;">
+            	    <div class="nav-title no-desktop"><strong>Studio Pym</strong> - Editoria e Comunicazione</div>
+            	    <ul id="main-nav">
+            	        <li><a class="back" href="#"><span data-hover="&laquo; Back">&laquo; Back</span></a></li>
+            	    </ul>
+            	</nav>
+            </header>
+			<div id="main" class="scene_element scene_element--fadein">
+                <!-- ........................................................... -->
+                <section class="gallery-container">
+    				<div class="gallery-content">
+    					<div><img src="assets/img/static/progetti-grafici/interni/BORGHESE/BORG_dettaglio.jpg" width="960" height="636" alt="BORG Dettaglio"></div>
+    					<div><img src="assets/img/static/progetti-grafici/interni/BORGHESE/BORG_dettaglio1.jpg" width="960" height="636" alt="BORG Dettaglio1"></div>
+    					<div><img src="assets/img/static/progetti-grafici/interni/BORGHESE/BORG_dettaglio2.jpg" width="960" height="636" alt="BORG Dettaglio2"></div>
+    					<div><img src="assets/img/static/progetti-grafici/interni/BORGHESE/BORG_interni.jpg" width="960" height="636" alt="BORG Interni"></div>
+    					<div><img src="assets/img/static/progetti-grafici/interni/BORGHESE/BORG_interni1.jpg" width="960" height="636" alt="BORG Interni1"></div>
+    				</div>
+    				<div class="slick-dots"></div>
+                </section>
+        		<div class="depth-wrapper">
+                <!-- ........................................................... -->
+        			<section class="detail">
+        				<div class="content jgrid">
+        					<div class="left-column">
+        			      	  <h2 class="tit2">100 RICETTE TRA GUSTO E ROCK’N’ROLL</h2>
+                            </div>
+                            <div class="right-column">
+        				        <p class="txt2">Alessandro Borghese, "Tu come lo fai?", Mondadori</p>
+                            </div>
+        			</section>
+                <!-- ........................................................... -->
+                </div>
+            </div>
+		</div>
+    <?php require('inc/footer.php'); ?>
+   </body>
+</html>
